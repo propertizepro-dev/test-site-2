ed6a9704dd1f6f68bbcc9597391680a0d6a90e98
Bejan <bejan512@gmail.com>
2023-06-14 01:37:23 -0400

    add any type to button

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-export default function Button({ children }) {
+export default function Button({ children }: any) {
   return (
     <div className="flex">
       <button className="font-bebas text-zinc-100 bg-indigo-800 px-6 py-3 text-2xl rounded-3xl hover:bg-black hover:border-white border-transparent border-2 transition ease-in-out duration-300">
